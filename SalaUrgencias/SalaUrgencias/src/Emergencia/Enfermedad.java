a4272ada69aebf9d6ed31bbb38084264e6040d40
Gabriel Del Valle <168558906+DV-Gabriel@users.noreply.github.com>
2024-05-13 09:18:26 -0500

    SalaUrgencias

---
+++ SalaUrgencias/SalaUrgencias/src/Emergencia/Enfermedad.java
@@ -0,0 +1,33 @@
+package Emergencia;
+
+public class Enfermedad {
+    private String tipoEnfermedad;
+    private String formaContagio;
+
+    public Enfermedad(String tipoEnfermedad,String formaContagio){
+        this.tipoEnfermedad = tipoEnfermedad;
+        this.formaContagio = formaContagio;
+    }
+
+    public String getTipoEnfermedad() {
+        return this.tipoEnfermedad;
+    }
+
+    public void setTipoEnfermedad(String tipoEnfermedad) {
+        this.tipoEnfermedad = tipoEnfermedad;
+    }
+
+
+    public String getFormaContagio() {
+        return this.formaContagio;
+    }
+
+    public void setFormaContagio(String formaContagio) {
+        this.formaContagio = formaContagio;
+    }
+
+    public String imprimirEnfermedad(){
+        return String.format("El paciente presenta una enfermedad %s, presume haberla adquirido por: %s. "
+                ,this.tipoEnfermedad,this.formaContagio);
+    }
+}
